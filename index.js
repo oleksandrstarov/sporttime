4b613291d7440834cfeabf000adbc6f228c7273f
Oleksandr Starov <oleksandr.starov@gmail.com>
2018-02-13 22:39:40 +0200

    Added serialport

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('App started');
+
+
+const SerialPort = require('serialport');
+
+const ByteLength = SerialPort.parsers.ByteLength
+const port = new SerialPort('COM4');
+const parser = port.pipe(new ByteLength({length: 8}));
+parser.on('data', console.log);
