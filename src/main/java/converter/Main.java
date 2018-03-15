c8b40e0ba2907193fb8d3a0036def6df58de5c42
Oleksandr Starov <oleksandr.starov@gmail.com>
2018-03-15 23:13:43 +0200

    Added view

--- src/main/java/converter/Main.java
+++ src/main/java/converter/Main.java
@@ -1,19 +1,28 @@
 package converter;
 
+import converter.controller.Controller;
 import converter.service.FileWatcher;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        createWatcher();
+        //createWatcher();
         System.out.println("watcher added");
-        initStage(primaryStage);
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("ST>>>MeOS");
+
+        initStage();
+        initApplicationView();
     }
 
 
@@ -21,15 +30,39 @@ public class Main extends Application {
         launch(args);
     }
 
-    private void initStage(Stage primaryStage) {
+    private void initStage() {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/converter.fxml"));
-            primaryStage.setTitle("Hello World");
-            primaryStage.setScene(new Scene(root, 300, 275));
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/fxml/converterBase.fxml"));
+            rootLayout = loader.load();
+
+            primaryStage.setScene(new Scene(rootLayout, 300, 275));
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initApplicationView() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/fxml/converter.fxml"));
+            Pane applicationView = loader.load();
+
+            rootLayout.setCenter(applicationView);
+
+            Controller controller = loader.getController();
+            controller.setMainApp(this);
+
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
 
     private void createWatcher() throws Exception{
--- src/main/java/converter/controller/Controller.java
+++ src/main/java/converter/controller/Controller.java
@@ -1,5 +1,23 @@
 package converter.controller;
 
+import converter.Main;
+import javafx.fxml.FXML;
+
 public class Controller {
+    Main mainApp;
+
+
+    public Controller() {
+
+    }
+
+    @FXML
+    private void initialize() {
+        System.out.println("init");
+    }
+
+    public void setMainApp(Main mainApp) {
+        this.mainApp = mainApp;
+    }
 
 }
