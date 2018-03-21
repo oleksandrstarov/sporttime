929046f8577c4e1a46c1ade92eb3d6d8c153d764
Oleksandr Starov <oleksandr.starov@gmail.com>
2018-03-21 19:05:09 +0200

    Added view

--- src/main/java/converter/Main.java
+++ src/main/java/converter/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -20,6 +21,7 @@ public class Main extends Application {
         System.out.println("watcher added");
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("ST>>>MeOS");
+        this.primaryStage.getIcons().add(new Image(getClass().getResource("/icon.png").toString()));
 
         initStage();
         initApplicationView();
@@ -35,8 +37,8 @@ public class Main extends Application {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("/fxml/converterBase.fxml"));
             rootLayout = loader.load();
-
-            primaryStage.setScene(new Scene(rootLayout, 300, 275));
+            Scene scene = new Scene(rootLayout, 400, 200);
+            primaryStage.setScene(scene);
             primaryStage.show();
 
         } catch (Exception e) {
--- src/main/java/converter/controller/Controller.java
+++ src/main/java/converter/controller/Controller.java
@@ -2,22 +2,82 @@ package converter.controller;
 
 import converter.Main;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class Controller {
     Main mainApp;
 
-
-    public Controller() {
-
-    }
+    FileChooser fileChooser;
 
     @FXML
-    private void initialize() {
-        System.out.println("init");
+    private TextField portNumberFiled;
+
+    @FXML
+    private TextField fileAddressField;
+
+    @FXML
+    private Button startBtn;
+
+    @FXML
+    private ProgressIndicator loader;
+
+    public Controller() {
+        fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("csv Files", "*.csv"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
     }
 
     public void setMainApp(Main mainApp) {
         this.mainApp = mainApp;
     }
 
+    @FXML
+    private void initialize() {
+        loader.setVisible(false);
+
+        System.out.println("init");
+        portNumberFiled.setText("10000");
+        fileAddressField.setText("");
+        validateInput();
+        fileAddressField.textProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("textfield changed from " + oldValue + " to " + newValue);
+            validateInput();
+        });
+
+        portNumberFiled.textProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("textfield changed from " + oldValue + " to " + newValue);
+            validateInput();
+        });
+    }
+
+    @FXML
+    private void selectFile() {
+        File selectedFile = fileChooser.showOpenDialog(mainApp.getPrimaryStage().getOwner());
+        if (selectedFile != null) {
+            fileAddressField.setText(selectedFile.getPath());
+        }
+    }
+
+    @FXML
+    private void startApp() {
+        startBtn.setDisable(true);
+        loader.setVisible(true);
+
+    }
+
+    @FXML
+    private void exitApp() {
+        System.exit(1);
+    }
+
+    private void validateInput() {
+        startBtn.setDisable(portNumberFiled.getText().length() == 0 || fileAddressField.getText().length() == 0);
+    }
 }
