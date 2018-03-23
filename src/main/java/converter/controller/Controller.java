7b89cdba04d06ed826141e51832db0c64f364619
Oleksandr Starov <oleksandr.starov@gmail.com>
2018-03-23 21:30:59 +0200

    Added info alert

--- src/main/java/converter/controller/Controller.java
+++ src/main/java/converter/controller/Controller.java
@@ -2,6 +2,7 @@ package converter.controller;
 
 import converter.Main;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
@@ -75,6 +76,16 @@ public class Controller {
     @FXML
     private void exitApp() {
         System.exit(1);
+    }
+
+    @FXML
+    private void showInfo() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("О приложени");
+        alert.setHeaderText("Приложение для ковертации данных ST в приграмму MeOS");
+        alert.setContentText("1.Запустите KuKuD.exe\n2.Проверьте, что путь к файлу совпадает\n3.Запустите MeOS и установите соответствующий порт для TCP соединения\n\n oleksandr.starov v.0.0.1");
+
+        alert.showAndWait();
     }
 
     private void validateInput() {
